d03457c798b5fcc0bc0b6896388d005dc3b452c7
Ilya Boyko <bkilya@gmail.com>
2023-12-06 18:34:23 +0100

    Subscription provider

---
+++ Provider.cs
@@ -0,0 +1,26 @@
+﻿using Pulumi.AzureNative;
+
+namespace Bk.Demo.Pulumi
+{
+    public static class ProviderFactory
+    {
+        public static Provider Create(string name, string subscriptionId, string clientId, string clientSecret)
+        {
+            return new Provider($"subscriptionProvider-{name}", new ProviderArgs()
+            {
+                SubscriptionId = subscriptionId,
+                ClientId = clientId,
+                ClientSecret = clientSecret
+            }); ;
+        }
+
+        public static Provider Create(string name, string subscriptionId, bool useManagedIdentiry)
+        {
+            return new Provider($"subscriptionProvider-{name}", new ProviderArgs()
+            {
+                SubscriptionId = subscriptionId,
+                UseMsi = useManagedIdentiry
+            }); ;
+        }
+    }
+}
